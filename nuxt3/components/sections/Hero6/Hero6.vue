ec581a7dae303309b5ca63017b512b25cb6a19e8
mlavalle <marc-aurele.lavalle@epitech.eu>
2023-07-29 22:39:27 +0200

    Hero 6 responsive

--- nuxt3/components/sections/Hero6/Hero6.vue
+++ nuxt3/components/sections/Hero6/Hero6.vue
@@ -1,9 +1,9 @@
 <template>
-  <section :class="{ 'flex-col-reverse': data?.positionHero6 === 'bot' }" class="flex flex-col justify-center py-10 h-full mb-10 sm:mb-0 max-w-[1200px] mx-auto gap-10">
+  <section :class="{ 'flex-col-reverse': data?.positionHero6 === 'bot' }" class="flex flex-col justify-center h-full mb-10 sm:mb-0 max-w-[1200px] mx-auto gap-10">
     <nuxt-img :src="`http://localhost:1337${data?.image.data?.attributes.url}`" alt=""
       class="w-full object-cover sm:h-[500px]" />
-    <div class="p-4 sm:p-0">
-      <div class="flex flex-col sm:flex-row bg-white rounded bg-opacity-60 sm:bg-transparent p-4 gap-6">
+    <div class="px-4 sm:px-0">
+      <div class="flex flex-col sm:flex-row bg-white rounded bg-opacity-60 sm:bg-transparent sm:p-4 sm:gap-6">
         <GradientTitle :title="data?.title" :level="1" />
         <div>
           <p>{{ data?.description }}</p>
