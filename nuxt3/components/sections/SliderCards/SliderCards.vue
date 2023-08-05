eed2c6c2b9bfc5b55faa762fcb0bf50750fdd535
mlavalle <marc-aurele.lavalle@epitech.eu>
2023-08-05 12:23:50 +0200

    Add pagination

--- nuxt3/components/sections/SliderCards/SliderCards.vue
+++ nuxt3/components/sections/SliderCards/SliderCards.vue
@@ -2,16 +2,10 @@
   <section class="flex flex-col justify-center h-full py-10 max-w-[1200px] mx-auto gap-3 p-4">
     <p>{{ data?.uptitle }}</p>
     <GradientTitle :title="data?.title" :level="2" class="w-full sm:w-2/3" />
-    <Swiper 
-    class="max-w-[1200px]"
-      :slides-per-view="3"
-      :space-between="20"
-      :pagination="{ clickable: true }"
-      :navigation="{prevEl: '.swiper-navigation-prev', nextEl: '.swiper-navigation-next'}"
-    >
+    <Swiper :modules="[SwiperPagination]" class="max-w-[1200px]" :slides-per-view="3" :space-between="20" :pagination="{ clickable: true }">
       <SwiperSlide v-for="(card, index) in data?.cards" :key="index">
-        <div class="shadow-xl h-auto">
-          <nuxt-img :src="`http://localhost:1337${card?.image.data?.attributes.url}`" alt=""/>
+        <div class="flex flex-col shadow-xl">
+          <nuxt-img class="h-48 object-cover" :src="`http://localhost:1337${card?.image.data?.attributes.url}`" alt="" />
           <div class="p-4">
             <GradientTitle :title="card?.title" :level="3" />
             <p>{{ card.description }}</p>
@@ -19,9 +13,6 @@
           </div>
         </div>
       </SwiperSlide>
-      <SwiperNavigation class="swiper-navigation-prev" slot="prev">qfffqs</SwiperNavigation>
-      <SwiperNavigation class="swiper-navigation-next" slot="next">qfsfs</SwiperNavigation>
-      <SwiperPagination></SwiperPagination>
     </Swiper>
   </section>
 </template>
