e6e30eccef9179194811e6333bd5dd1ed729a985
mlavalle <marc-aurele.lavalle@epitech.eu>
2023-08-13 12:45:56 +0200

    Header

--- nuxt3/components/Header.vue
+++ nuxt3/components/Header.vue
@@ -1,15 +1,48 @@
 <template>
-    <header class="bg-white p-4 shadow-sm">
-        <div class="container mx-auto flex justify-between items-center">
-            <NuxtLink to="/"><nuxt-img v-if="logoUrl" :src="logoUrl" alt="Logo" class="h-10" /></NuxtLink>
-            <nav>
-                <ul class="flex space-x-4">
+    <header>
+        <nav class="bg-white">
+            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div class="relative flex h-16 items-center justify-between">
+                    <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        <NuxtLink
+                            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-white"
+                            aria-controls="mobile-menu" aria-expanded="false">
+                            <span class="absolute -inset-0.5"></span>
+                            <span class="sr-only">Open main menu</span>
+                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        </NuxtLink>
+                    </div>
+                    <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div class="flex flex-shrink-0 items-center">
+                            <NuxtLink to="/"><nuxt-img v-if="logoUrl" :src="logoUrl" alt="Logo" class="h-10" /></NuxtLink>
+                        </div>
+                        <div class="hidden sm:ml-6 sm:block">
+                            <ul class="flex space-x-4">
+                                <li v-for="link in links" :key="link.id">
+                                    <NuxtLink :to="link.link"
+                                        class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">{{
+                                            link.label }}</NuxtLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="sm:hidden" id="mobile-menu">
+                <ul class="space-y-1 px-2 pb-3 pt-2">
                     <li v-for="link in links" :key="link.id">
-                        <NuxtLink :to="link.link">{{ link.label }}</NuxtLink>
+                        <NuxtLink :to="link.link"
+                            class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 font-medium">
+                            {{ link.label }}</NuxtLink>
                     </li>
                 </ul>
-            </nav>
-        </div>
+            </div>
+        </nav>
     </header>
 </template>
 
