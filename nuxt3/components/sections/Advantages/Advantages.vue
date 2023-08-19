1cbc9cde4cc140b1044f97980e07f49cecdbdacd
mlavalle <marc-aurele.lavalle@epitech.eu>
2023-08-19 22:12:21 +0200

    advantage design

--- nuxt3/components/sections/Advantages/Advantages.vue
+++ nuxt3/components/sections/Advantages/Advantages.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="flex justify-center h-full py-10 max-w-[1200px] mx-auto gap-10">
-      <div v-for="(item, index) in data.advantage" :key="index" class="flex flex-col items-center text-center">
+  <section class="flex justify-around h-full py-10 max-w-[1200px] mx-auto gap-10">
+      <div v-for="(item, index) in data?.advantage" :key="index" class="flex flex-col items-center text-center">
         <nuxt-icon :name="item.icon" class="mb-3"/>
         <p class="font-semibold">{{ item.title }}</p>
         <p>{{ item.description }}</p>
